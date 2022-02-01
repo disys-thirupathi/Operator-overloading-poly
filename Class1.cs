10398172b635f266172e3c05182ff6bb674c7e4d
thirupathi <thirupathi.narayanan@disys.com>
2022-02-01 18:17:39 +0530

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+public class Class1
+{
+	public class interest
+    {
+		public double total;
+		public mybank(int cost, double rate)
+        {
+			total = cost + (rate * rate);
+			return total;
+        }
+		public static mybank operator +(mybank b)
+        {
+			b.total = +b.total;
+        }
+    }
+	public Class1()
+	{
+		public static void Main(string args[])
+        {
+			interest i = new interest();
+			double amount = +i.mybank(50000, 0.5);
+			Console.WriteLine("Interest amount " + amount);
+        }
+	}
+}
